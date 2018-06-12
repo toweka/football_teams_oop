45db10405c22e0729c5acc988ba4c55903b7dd6f
TECH-A58 <tech-a58@TECH-A58s-MacBook-Air.local>
2018-06-12 19:26:51 +0100

    Created initial class of top football teams

---
+++ football_teams.rb
@@ -0,0 +1,11 @@
+class TopFootballTeams
+
+  def self.licence
+    puts "A top football team must be fully lisenced by it's national governing body."
+  end
+
+  def self.pedigree
+    puts "Must be actively participating in the highest national division as well as the champions league"
+  end
+
+end
